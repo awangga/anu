0df872df93c020437cf068040820581e86047e45
Rolly Maulana Awangga <rolly@awang.ga>
2016-10-25 12:39:27 +0700

    add lan scan

--- anu.py
+++ anu.py
@@ -8,5 +8,21 @@ from scapy.all import ARP,Ether,sendp
 
 class Anu(object):
 	def sendArp(self,mac_rec,ip_rec,ip_target):	
-		paket = Ether()/ARP(op="who-has",hwsrc=mac_rec,psrc=ip_rec,pdst=ip_target)
- 		sendp(paket)
+		try:
+			paket = Ether()/ARP(op="who-has",hwsrc=mac_rec,psrc=ip_rec,pdst=ip_target)
+ 			sendp(paket)
+			return True
+		except AttributeError:
+			return False
+	def scanNet(self,net_add,begin,end):
+		net = net_add[:-1]
+		result = ''
+		total = 0
+		for num in range(begin,end):
+			ip = net+str(num)
+			cek = self.sendArp('00:00:00:00:00:00','169.254.254.254',ip)
+			if cek:
+				result=result+ip+' , '
+				total += 1
+		return str(total)+' Alive Node: '+result
+			
